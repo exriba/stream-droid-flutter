2d7387047276e2e34013fc952f8e3f54ea021d16
exriba <44509247+exriba@users.noreply.github.com>
2024-03-05 01:06:48 -0500

    create statistics view

---
+++ lib/statistic/statistics_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:stream_droid_app/layout/base_view.dart';
+
+class StatisticsView extends StatelessWidget with BaseView {
+  const StatisticsView({super.key});
+
+  @override
+  String get name => "Statistics";
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container();
+  }
+}
