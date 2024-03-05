a958b522afd215c9bf4f746852e2897bca281390
exriba <44509247+exriba@users.noreply.github.com>
2024-03-05 01:06:16 -0500

    create settings view

--- lib/setting/settings_view.dart
+++ lib/setting/settings_view.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:stream_droid_app/layout/base_view.dart';
 
-class SettingsView extends StatelessWidget {
+class SettingsView extends StatelessWidget with BaseView {
   const SettingsView({super.key});
+
+  @override
+  String get name => "Settings";
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    throw UnimplementedError();
+    return Container();
   }
 }
