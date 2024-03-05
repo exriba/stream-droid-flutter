f5136701bba01b441f678b2b25704ff11f43fdfe
exriba <44509247+exriba@users.noreply.github.com>
2024-03-05 01:04:20 -0500

    create media view

---
+++ lib/media/media_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class MediaView extends StatelessWidget {
+  const MediaView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
