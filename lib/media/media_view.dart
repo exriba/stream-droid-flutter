1d9136ffc42ccbee1ffd36186137c7476d75092e
exriba <44509247+exriba@users.noreply.github.com>
2024-03-05 01:10:25 -0500

    adding media view to navigation

--- lib/media/media_view.dart
+++ lib/media/media_view.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:stream_droid_app/layout/base_view.dart';
+import 'package:stream_droid_app/layout/navigation_view.dart';
 
-class MediaView extends StatelessWidget {
+class MediaView extends StatelessWidget with BaseView {
   const MediaView({super.key});
 
   @override
+  String get name => "Media";
+
+  @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return NavigationView(
+      baseView: this,
+      child: Container(),
+    );
   }
 }
