aa00af0e616abe841f42e54468a37a26cd4db084
exriba <44509247+exriba@users.noreply.github.com>
2024-04-04 13:09:38 -0400

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,14 +8,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:stream_droid_app/app.dart';
-import 'package:stream_droid_app/context/user_context.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(App(
-      userContext: UserContext(),
-    ));
+    await tester.pumpWidget(const App());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
