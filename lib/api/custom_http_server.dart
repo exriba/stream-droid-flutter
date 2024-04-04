d7c387b6c31f8b571c6424b7e4a1b45d1d11ab38
exriba <44509247+exriba@users.noreply.github.com>
2024-04-04 11:20:38 -0400

    Update custom_http_server.dart
    Updated http server to serve static pages on user authentication.

--- lib/api/custom_http_server.dart
+++ lib/api/custom_http_server.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'package:flutter/services.dart' show rootBundle;
 import 'package:stream_droid_app/common/constants.dart' as constants;
 
 abstract class ICustomHttpServer {
@@ -20,13 +21,45 @@ class CustomHttpServer implements ICustomHttpServer {
       final cookies = request.cookies.where(
         (element) => element.name == constants.appName,
       );
-
-      if (cookies.isNotEmpty) {
-        final cookie = cookies.first;
-        await onData(value: cookie.value);
+      switch (request.uri.path) {
+        case '/':
+          if (cookies.isNotEmpty) {
+            final cookie = cookies.first;
+            final content =
+                await rootBundle.loadString('assets/html/Success.html');
+            request.response.statusCode = HttpStatus.ok;
+            request.response.headers
+                .set('Content-Type', 'text/html; charset=utf-8');
+            request.response.write(content);
+            onData(value: cookie.value);
+          }
+        case '/favicon.ico':
+          final image = File("assets/icons/favicon.ico");
+          final rawImage = await image.readAsBytes();
+          request.response.statusCode = HttpStatus.ok;
+          request.response.headers.set('Content-Type', 'image/x-icon');
+          request.response.headers.set('Content-Length', rawImage.length);
+          request.response.add(rawImage);
+        case '/app-icon.png':
+          final image = File("assets/images/app-icon.png");
+          final rawImage = await image.readAsBytes();
+          request.response.statusCode = HttpStatus.ok;
+          request.response.headers.set('Content-Type', 'image/png');
+          request.response.headers.set('Content-Length', rawImage.length);
+          request.response.add(rawImage);
+        case '/manifest.json':
+          final content =
+              await rootBundle.loadString('assets/html/manifest.json');
+          request.response.statusCode = HttpStatus.ok;
+          request.response.headers.set('Content-Type', 'application/json');
+          request.response.write(content);
+        default:
+          final content = await rootBundle.loadString('assets/html/Error.html');
+          request.response.statusCode = HttpStatus.methodNotAllowed;
+          request.response.headers
+              .set('Content-Type', 'text/html; charset=utf-8');
+          request.response.write(content);
       }
-
-      request.response.write("Close!");
       await request.response.close();
     }
   }
