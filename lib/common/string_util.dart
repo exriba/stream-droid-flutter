97bcea775b47d7402d8e07056750d92ea0e027af
exriba <44509247+exriba@users.noreply.github.com>
2024-04-03 23:48:29 -0400

    Delete string_util.dart
    unnecessary extension

--- lib/common/string_util.dart
+++
@@ -1,8 +0,0 @@
-extension StringUtil on String {
-  String withCapitalizedFirstLetter() {
-    if (length == 0) {
-      throw const FormatException("String length must be greater than 0.");
-    }
-    return this[0].toUpperCase() + substring(1);
-  }
-}
