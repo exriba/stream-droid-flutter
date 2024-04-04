6644262546d7ccfa6fedb2608ded6f47595b5c57
exriba <44509247+exriba@users.noreply.github.com>
2024-04-03 23:51:22 -0400

    Create dependency_manager.dart
    Simple manager to register and retrieve singleton and transient components.

---
+++ lib/util/dependency_manager.dart
@@ -0,0 +1,14 @@
+import 'package:get_it/get_it.dart';
+import 'package:stream_droid_app/api/custom_http_client.dart';
+import 'package:stream_droid_app/api/custom_http_server.dart';
+import 'package:stream_droid_app/util/secure_storage.dart';
+
+class DependencyManager {
+  static GetIt getIt = GetIt.instance;
+
+  static void configure() {
+    getIt.registerSingleton<ISecureStorage>(SecureStorage());
+    getIt.registerSingleton<ICustomHttpClient>(CustomHttpClient());
+    getIt.registerSingleton<ICustomHttpServer>(CustomHttpServer());
+  }
+}
