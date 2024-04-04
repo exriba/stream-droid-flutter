62d56f2a742b0e0ba51fd0ef14c5b28e89f8e746
exriba <44509247+exriba@users.noreply.github.com>
2024-04-03 23:58:24 -0400

    Update login_view.dart
    Converted LoginView into a stateful widget that initializes an http server to handle authentication redirects from the browser back to the application.
    
    The server is immediately disposed following a successful authentication.

--- lib/auth/login_view.dart
+++ lib/auth/login_view.dart
@@ -1,68 +1,97 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:stream_droid_app/api/custom_http_client.dart';
+import 'package:stream_droid_app/api/custom_http_server.dart';
+import 'package:stream_droid_app/common/types.dart';
+import 'package:stream_droid_app/util/dependency_manager.dart';
 import 'package:stream_droid_app/layout/app_view.dart';
 import 'package:stream_droid_app/context/user_context.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:window_manager/window_manager.dart';
 
-final class LoginView extends StatelessWidget {
-  const LoginView({super.key});
+final class LoginView extends StatefulWidget {
+  const LoginView({super.key, required this.userContext});
+  final UserContext userContext;
 
-  Future<void> handleLogin(UserContext userContext) async {
-    await windowManager.hide();
-    userContext.onLogin();
+  @override
+  State<StatefulWidget> createState() => _LoginView();
+}
 
-    const windowSize = Size(1280, 720);
-    await windowManager.setSize(windowSize);
-    await windowManager.center();
-    await windowManager.show();
+final class _LoginView extends State<LoginView> {
+  late ICustomHttpServer httpServer;
+  late ICustomHttpClient httpClient;
+
+  @override
+  void initState() {
+    super.initState();
+    httpServer = DependencyManager.getIt.get<ICustomHttpServer>();
+    httpClient = DependencyManager.getIt.get<ICustomHttpClient>();
+    httpServer.initializeHttpServer(onData: ({required String value}) async {
+      await windowManager.hide();
+      await widget.userContext.onLogin(value);
+
+      const windowSize = Size(1280, 720);
+      await windowManager.setSize(windowSize);
+      await windowManager.center();
+      await windowManager.show();
+    });
+  }
+
+  Future<void> handleLogin() async {
+    final redirectUrl = await httpClient.get(urlFragment: UrlFragment.meLogin);
+    final authorizationUrl = Uri.parse(redirectUrl);
+    if (await canLaunchUrl(authorizationUrl)) {
+      await launchUrl(authorizationUrl);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<UserContext>(builder: (context, userContext, child) {
-      return AppView(
-        child: Container(
-          alignment: Alignment.center,
-          color: const Color.fromRGBO(33, 33, 33, 1),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Image.asset(
-                "assets/icons/app-icon.png",
-                height: 128,
-                width: 128,
-              ),
-              SizedBox(
-                height: 50,
-                width: 150,
-                child: TextButton.icon(
-                  style: ElevatedButton.styleFrom(
-                      elevation: 3,
-                      shadowColor: Colors.black,
-                      backgroundColor: Colors.purple[400],
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12))),
-                  icon: SvgPicture.asset(
-                    'assets/twitch.svg',
-                    height: 30,
-                    width: 30,
-                    colorFilter:
-                        const ColorFilter.mode(Colors.white, BlendMode.srcIn),
-                  ),
-                  label: Text(
-                    'Login',
-                    style: Theme.of(context).textTheme.bodyMedium,
-                  ),
-                  onPressed: () async {
-                    await handleLogin(userContext);
-                  },
+    return AppView(
+      child: Container(
+        alignment: Alignment.center,
+        color: const Color.fromRGBO(33, 33, 33, 1),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Image.asset(
+              "assets/icons/app-icon.png",
+              height: 128,
+              width: 128,
+            ),
+            SizedBox(
+              height: 50,
+              width: 150,
+              child: TextButton.icon(
+                style: ElevatedButton.styleFrom(
+                    elevation: 3,
+                    shadowColor: Colors.black,
+                    backgroundColor: Colors.purple[400],
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12))),
+                icon: SvgPicture.asset(
+                  'assets/twitch.svg',
+                  height: 30,
+                  width: 30,
+                  colorFilter:
+                      const ColorFilter.mode(Colors.white, BlendMode.srcIn),
                 ),
+                label: Text(
+                  'Login',
+                  style: Theme.of(context).textTheme.bodyMedium,
+                ),
+                onPressed: handleLogin,
               ),
-            ],
-          ),
+            ),
+          ],
         ),
-      );
-    });
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    httpServer.dispose();
   }
 }
