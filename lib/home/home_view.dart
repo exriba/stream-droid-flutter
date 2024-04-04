015729dbfbe4c0024d308efbea7a10e3a8aecf52
exriba <44509247+exriba@users.noreply.github.com>
2024-04-04 00:01:14 -0400

    Update home_view.dart
    Returns the appropriate view depending on the user's authentication status.

--- lib/home/home_view.dart
+++ lib/home/home_view.dart
@@ -7,12 +7,23 @@ import 'package:stream_droid_app/context/user_context.dart';
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
+  Future<bool> _verifyUserAuthentication(UserContext userContext) async {
+    return await userContext.isAuthenticated();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Consumer<UserContext>(builder: (context, userContext, child) {
-      return userContext.authenticated
-          ? const DashboardView()
-          : const LoginView();
+      return FutureBuilder<bool>(
+          future: _verifyUserAuthentication(userContext),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done &&
+                snapshot.data == true) {
+              return const DashboardView();
+            }
+
+            return LoginView(userContext: userContext);
+          });
     });
   }
 }
