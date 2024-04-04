41881a67f0ad5c6959ad2b37c36a9cfd67783e16
exriba <44509247+exriba@users.noreply.github.com>
2024-04-04 13:10:11 -0400

    Consolidate window resizing

--- lib/home/home_view.dart
+++ lib/home/home_view.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:stream_droid_app/layout/loading_view.dart';
+import 'package:window_manager/window_manager.dart';
 import 'package:stream_droid_app/auth/login_view.dart';
-import 'package:stream_droid_app/dashboard/dashboard_view.dart';
 import 'package:stream_droid_app/context/user_context.dart';
+import 'package:stream_droid_app/dashboard/dashboard_view.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
+  final maximumSize = const Size(1280, 720);
+  final minimumSize = const Size(900, 600);
 
   Future<bool> _verifyUserAuthentication(UserContext userContext) async {
-    return await userContext.isAuthenticated();
+    final authenticated = await userContext.isAuthenticated();
+    final size = authenticated ? maximumSize : minimumSize;
+    await windowManager.setSize(size);
+    await windowManager.center();
+    return authenticated;
   }
 
   @override
@@ -17,12 +25,13 @@ class HomeView extends StatelessWidget {
       return FutureBuilder<bool>(
           future: _verifyUserAuthentication(userContext),
           builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.done &&
-                snapshot.data == true) {
-              return const DashboardView();
+            if (snapshot.connectionState != ConnectionState.done) {
+              return const LoadingView();
             }
 
-            return LoginView(userContext: userContext);
+            return snapshot.data == true
+                ? const DashboardView()
+                : LoginView(userContext: userContext);
           });
     });
   }
