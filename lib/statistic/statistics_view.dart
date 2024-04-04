c6fc8ee840813f4fe013581eda30b4ec8bc46878
exriba <44509247+exriba@users.noreply.github.com>
2024-04-04 00:07:27 -0400

    Update statistics_view.dart
    Updated Statistics view to communicate with backend api and display real data.

--- lib/statistic/statistics_view.dart
+++ lib/statistic/statistics_view.dart
@@ -1,10 +1,25 @@
+import 'dart:convert';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:stream_droid_app/api/custom_http_client.dart';
+import 'package:stream_droid_app/common/hex_color.dart';
 import 'package:stream_droid_app/common/types.dart';
 import 'package:stream_droid_app/layout/navigation_view.dart';
+import 'package:stream_droid_app/util/dependency_manager.dart';
 
 class StatisticsView extends StatelessWidget {
   const StatisticsView({super.key});
+
+  Future<List<RedeemRedemption>> _fetchChannelRedeemRedemptions() async {
+    final httpClient = DependencyManager.getIt.get<ICustomHttpClient>();
+    final data = await httpClient.get(urlFragment: UrlFragment.redemptions);
+    final parsed = (jsonDecode(data) as List).cast<Map<String, dynamic>>();
+    return parsed.isEmpty
+        ? <RedeemRedemption>[]
+        : parsed
+            .map<RedeemRedemption>((json) => RedeemRedemption.fromJson(json))
+            .toList();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,32 +27,40 @@ class StatisticsView extends StatelessWidget {
       viewDestination: ViewDestination.statistics,
       child: Padding(
         padding: const EdgeInsets.all(20),
-        child: LayoutBuilder(
-          builder: (context, constraints) {
-            final shortestSide = constraints.biggest.shortestSide;
-            return PieChart(
-              PieChartData(
-                sections: [
-                  PieChartSectionData(
-                    color: Colors.red,
-                    value: 50,
-                    radius: shortestSide / 2,
-                  ),
-                  PieChartSectionData(
-                    color: Colors.green,
-                    value: 30,
-                    radius: shortestSide / 2,
-                  ),
-                  PieChartSectionData(
-                    color: Colors.purple,
-                    value: 20,
-                    radius: shortestSide / 2,
-                  ),
-                ],
-              ),
-            );
-          },
-        ),
+        child: FutureBuilder<List<RedeemRedemption>>(
+            future: _fetchChannelRedeemRedemptions(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                return LayoutBuilder(builder: (context, constraints) {
+                  final shortestSide = constraints.biggest.shortestSide;
+                  return PieChart(
+                    PieChartData(
+                      sections: snapshot.data!
+                          .map((x) => PieChartSectionData(
+                                color: HexColor.fromHex(x.color),
+                                value: x.number,
+                                badgeWidget: Tooltip(
+                                  message: x.name,
+                                  child: Text(
+                                    '${x.number.toString()}%',
+                                    style:
+                                        Theme.of(context).textTheme.bodyMedium,
+                                  ),
+                                ),
+                                showTitle: false,
+                                radius: shortestSide / 2,
+                              ))
+                          .toList(),
+                    ),
+                  );
+                });
+              }
+              return const Align(
+                child: CircularProgressIndicator(
+                  color: Colors.grey,
+                ),
+              );
+            }),
       ),
     );
   }
