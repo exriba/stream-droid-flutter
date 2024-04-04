7fa43c5493d4ad3b9a55af2fbfa58b6bd205118e
exriba <44509247+exriba@users.noreply.github.com>
2024-04-03 23:48:57 -0400

    Create constants.dart
    common constants and environmental variables.

---
+++ lib/common/constants.dart
@@ -0,0 +1,5 @@
+const String appName = String.fromEnvironment("app_name");
+const String referer = String.fromEnvironment("referer");
+const int serverPort = int.fromEnvironment("server_port");
+const String apiBaseUrl = String.fromEnvironment("api_base_url");
+const String serverAddress = String.fromEnvironment("server_address");
