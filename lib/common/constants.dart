55326b03f340851d104eb7243841f30a94e5b450
exriba <44509247+exriba@users.noreply.github.com>
2024-04-08 19:01:35 -0400

    Update constants.dart
    renamed variables

--- lib/common/constants.dart
+++ lib/common/constants.dart
@@ -2,5 +2,6 @@ const String appName = String.fromEnvironment("app_name");
 const int hostPort = int.fromEnvironment("host_port");
 const String hostName = String.fromEnvironment("host_name");
 const String hostAddress = String.fromEnvironment("host_address");
-const String serverHubAddress = String.fromEnvironment("server_hub_address");
 const String serverBaseAddress = String.fromEnvironment("server_base_address");
+const String serverEventAddress =
+    String.fromEnvironment("server_event_address");
