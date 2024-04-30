124f743743eda833089309f04e222cd6b3a03b98
exriba <44509247+exriba@users.noreply.github.com>
2024-04-30 17:23:17 -0400

    Update navigation_view.dart
    * Removed unnecessary reference to UserContext consumer.
    * Removed color repetition.

--- lib/layout/navigation_view.dart
+++ lib/layout/navigation_view.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:stream_droid_app/common/types.dart';
-import 'package:stream_droid_app/context/user_context.dart';
+import 'package:stream_droid_app/context/theme_context.dart';
 import 'package:stream_droid_app/dashboard/dashboard_view.dart';
 import 'package:stream_droid_app/layout/logout_button.dart';
 import 'package:stream_droid_app/media/media_view.dart';
@@ -48,84 +47,82 @@ class _NavigationView extends State<NavigationView> {
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<UserContext>(builder: (context, userContext, child) {
-      return Row(
-        children: [
-          Container(
-            color: Colors.grey[700],
-            child: Column(
-              children: [
-                Flexible(
-                  flex: 9,
-                  child: NavigationRail(
-                    extended: false,
-                    selectedIndex: viewDestination.index,
-                    indicatorColor: Colors.grey,
-                    backgroundColor: Colors.transparent,
-                    destinations: _navigationIcons
-                        .map(
-                          (item) => NavigationRailDestination(
-                            padding: const EdgeInsets.only(top: 30),
-                            icon: Icon(item),
-                            label: const SizedBox.shrink(),
-                          ),
-                        )
-                        .toList(),
-                    onDestinationSelected: _navigateToView,
-                  ),
-                ),
-                const Flexible(
-                  flex: 1,
-                  child: LogoutButton(),
-                ),
-              ],
-            ),
-          ),
-          Expanded(
-            flex: 1,
-            child: LayoutBuilder(builder: (context, constraints) {
-              return Container(
-                height: constraints.maxHeight,
-                decoration: const BoxDecoration(
-                  color: Color.fromRGBO(33, 33, 33, 1),
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    // TODO: Convert to custom widget. Add Text animation
-                    Flexible(
-                      flex: 0,
-                      child: Container(
-                        width: constraints.maxWidth / 2,
-                        margin: const EdgeInsets.only(top: 10, bottom: 10),
-                        decoration: BoxDecoration(
-                          color: Colors.grey[700],
-                          borderRadius: const BorderRadius.only(
-                            topRight: Radius.circular(10),
-                            bottomRight: Radius.circular(10),
-                          ),
+    return Row(
+      children: [
+        Container(
+          color: ThemeContext.defaultNavigationColor,
+          child: Column(
+            children: [
+              Flexible(
+                flex: 9,
+                child: NavigationRail(
+                  extended: false,
+                  selectedIndex: viewDestination.index,
+                  indicatorColor: Colors.grey,
+                  backgroundColor: Colors.transparent,
+                  destinations: _navigationIcons
+                      .map(
+                        (item) => NavigationRailDestination(
+                          padding: const EdgeInsets.only(top: 30),
+                          icon: Icon(item),
+                          label: const SizedBox.shrink(),
                         ),
-                        child: Padding(
-                          padding: const EdgeInsets.only(left: 10),
-                          child: Text(
-                            viewDestination.label,
-                            style: Theme.of(context).textTheme.bodyLarge,
-                          ),
+                      )
+                      .toList(),
+                  onDestinationSelected: _navigateToView,
+                ),
+              ),
+              const Flexible(
+                flex: 1,
+                child: LogoutButton(),
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: LayoutBuilder(builder: (context, constraints) {
+            return Container(
+              height: constraints.maxHeight,
+              decoration: const BoxDecoration(
+                color: Colors.transparent,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // TODO: Convert to custom widget. Add Text animation
+                  Flexible(
+                    flex: 0,
+                    child: Container(
+                      width: constraints.maxWidth / 2,
+                      margin: const EdgeInsets.only(top: 10, bottom: 10),
+                      decoration: BoxDecoration(
+                        color: Colors.grey[700],
+                        borderRadius: const BorderRadius.only(
+                          topRight: Radius.circular(10),
+                          bottomRight: Radius.circular(10),
+                        ),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 10),
+                        child: Text(
+                          viewDestination.label,
+                          style: Theme.of(context).textTheme.bodyLarge,
                         ),
                       ),
                     ),
-                    Flexible(
-                      flex: 1,
-                      child: view,
-                    ),
-                  ],
-                ),
-              );
-            }),
-          ),
-        ],
-      );
-    });
+                  ),
+                  Flexible(
+                    flex: 1,
+                    child: view,
+                  ),
+                ],
+              ),
+            );
+          }),
+        ),
+      ],
+    );
   }
 
   @override
