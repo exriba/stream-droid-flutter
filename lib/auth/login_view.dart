c01ce9e6b8ca43209b3a628cab1116a084c4f3e7
exriba <44509247+exriba@users.noreply.github.com>
2025-11-27 20:50:48 -0500

    Refactor Login widget
    * Removed usercontext reference. widgets should not receive context through the constructor. Instead they should access their own build context.

--- lib/auth/login_view.dart
+++ lib/auth/login_view.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
 import 'package:stream_droid_app/api/custom_http_client.dart';
 import 'package:stream_droid_app/api/custom_http_server.dart';
 import 'package:stream_droid_app/common/types.dart';
+import 'package:stream_droid_app/context/theme_context.dart';
 import 'package:stream_droid_app/util/dependency_manager.dart';
 import 'package:stream_droid_app/context/user_context.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 final class LoginView extends StatefulWidget {
-  const LoginView({super.key, required this.userContext});
-  final UserContext userContext;
+  const LoginView({super.key});
 
   @override
   State<StatefulWidget> createState() => _LoginView();
@@ -24,9 +26,6 @@ final class _LoginView extends State<LoginView> {
     super.initState();
     httpServer = DependencyManager.getIt.get<ICustomHttpServer>();
     httpClient = DependencyManager.getIt.get<ICustomHttpClient>();
-    httpServer.initializeHttpServer(onData: ({required String value}) async {
-      await widget.userContext.onLogin(value);
-    });
   }
 
   Future<void> handleLogin() async {
@@ -39,40 +38,55 @@ final class _LoginView extends State<LoginView> {
 
   @override
   Widget build(BuildContext context) {
-    return Align(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Image.asset(
-            "assets/images/app-icon.png",
-            height: 128,
-            width: 128,
-          ),
-          SizedBox(
-            height: 50,
-            width: 150,
-            child: TextButton.icon(
-              style: ElevatedButton.styleFrom(
-                  elevation: 3,
-                  shadowColor: Colors.black,
-                  backgroundColor: Colors.purple[400],
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12))),
-              icon: SvgPicture.asset(
-                'assets/images/twitch.svg',
-                height: 30,
-                width: 30,
-                colorFilter:
-                    const ColorFilter.mode(Colors.white, BlendMode.srcIn),
-              ),
-              label: Text(
-                'Login',
-                style: Theme.of(context).textTheme.bodyMedium,
-              ),
-              onPressed: handleLogin,
+    final themeContext = context.read<ThemeContext>();
+    httpServer.initializeHttpServer(onData: ({required String value}) async {
+      final userContext = context.read<UserContext>();
+      await userContext.onLogin(value);
+
+      if (context.mounted) {
+        context.go(ViewRoute.dashboard.route);
+      }
+    });
+
+    return Scaffold(
+      backgroundColor: themeContext.backgroundColor,
+      body: Align(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Image.asset(
+              "assets/images/app-icon.png",
+              height: 128,
+              width: 128,
             ),
-          ),
-        ],
+            SizedBox(
+              height: 50,
+              width: 150,
+              child: TextButton.icon(
+                style: ElevatedButton.styleFrom(
+                    elevation: 3,
+                    shadowColor: Colors.black,
+                    backgroundColor: Colors.purple[400],
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12))),
+                icon: SvgPicture.asset(
+                  'assets/images/twitch.svg',
+                  height: 30,
+                  width: 30,
+                  colorFilter:
+                      const ColorFilter.mode(Colors.white, BlendMode.srcIn),
+                ),
+                label: Text(
+                  'Login',
+                  style: Theme.of(context).textTheme.bodyMedium,
+                ),
+                onPressed: () async {
+                  await handleLogin();
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
