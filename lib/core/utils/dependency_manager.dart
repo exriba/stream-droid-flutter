706ab9337fed2bcba5825722499fec2cb861e49e
exriba <44509247+exriba@users.noreply.github.com>
2025-12-09 08:50:24 -0500

    Removed httpclient and implemented grpc channel and client

--- lib/core/utils/dependency_manager.dart
+++ lib/core/utils/dependency_manager.dart
@@ -2,6 +2,7 @@ import 'package:get_it/get_it.dart';
 import 'package:stream_droid_app/core/network/droid_client.dart';
 import 'package:stream_droid_app/core/utils/local_storage.dart';
 import 'package:stream_droid_app/core/utils/secure_storage.dart';
+import 'package:stream_droid_app/domain/services/auth_interceptor.dart';
 import 'package:stream_droid_app/domain/services/redeem_service.dart';
 import 'package:stream_droid_app/domain/services/user_service.dart';
 
@@ -11,11 +12,14 @@ class DependencyManager {
   static void configure() {
     getIt.registerSingleton<ILocalStorage>(LocalStorage());
     getIt.registerSingleton<ISecureStorage>(SecureStorage());
+    getIt.registerSingleton<AuthInterceptor>(
+      AuthInterceptor(getIt<ISecureStorage>()),
+    );
     getIt.registerSingleton<IDroidClient>(
       DroidClient(getIt<ISecureStorage>()),
     );
     getIt.registerSingleton<UserService>(
-      UserService(getIt<ISecureStorage>(), getIt<IDroidClient>()),
+      UserService(getIt<ISecureStorage>(), getIt<AuthInterceptor>()),
     );
     getIt.registerSingleton<RedeemService>(
       RedeemService(getIt<IDroidClient>()),
--- lib/domain/services/user_service.dart
+++ lib/domain/services/user_service.dart
@@ -1,28 +1,64 @@
-import 'package:stream_droid_app/core/network/droid_client.dart';
-import 'package:stream_droid_app/core/utils/types.dart';
 import 'package:stream_droid_app/core/utils/secure_storage.dart';
 import 'package:stream_droid_app/core/constants/constants.dart' as constants;
+import 'package:grpc/grpc.dart';
+import 'package:stream_droid_app/domain/generated/common/user.pbenum.dart';
+import 'package:stream_droid_app/domain/generated/google/protobuf/empty.pb.dart';
+import 'package:stream_droid_app/domain/generated/service/userservice.pbgrpc.dart';
+import 'package:stream_droid_app/domain/services/auth_interceptor.dart';
+import 'package:uuid/uuid.dart';
 
+// TODO: handle errors for all these methods.
 class UserService {
-  UserService(this._secureStorage, this._httpClient);
+  UserService(this._secureStorage, AuthInterceptor authInterceptor) {
+    final channel = ClientChannel(
+      constants.serverName,
+      port: constants.serverPort,
+      options: const ChannelOptions(
+        credentials: ChannelCredentials.insecure(),
+        // TODO: Use secure credentials for production
+      ),
+    );
+    _client = GrpcUserServiceClient(channel, interceptors: [authInterceptor]);
+  }
   final ISecureStorage _secureStorage;
-  final IDroidClient _httpClient;
+  final Uuid uuid = const Uuid();
+  late GrpcUserServiceClient _client;
+  late String _sessionId = '';
 
   Future<String> authorizationUrl() async {
-    return await _httpClient.get(urlFragment: UrlFragment.meLogin);
+    _sessionId = uuid.v4();
+    final sessionRequest = SessionRequest(sessionId: _sessionId);
+    final sessionResponse = await _client.login(sessionRequest);
+    return sessionResponse.authorizationUrl;
+  }
+
+  Future<void> monitorAuthentication() async {
+    final sessionRequest = SessionRequest(sessionId: _sessionId);
+    final response = _client.authenticationSession(sessionRequest);
+
+    await for (final update in response) {
+      if (update.status == SessionStatus_Status.ERROR) {
+        print(update.message);
+        break;
+      }
+
+      if (update.status == SessionStatus_Status.AUTHORIZED) {
+        await _secureStorage.write(
+            key: constants.appName, value: update.accessToken);
+        break;
+      }
+
+      await Future.delayed(const Duration(milliseconds: 1000));
+    }
   }
 
   Future<bool> isAuthenticated() async {
-    final data = await _httpClient.get(urlFragment: UrlFragment.me);
-    return data.isNotEmpty;
-  }
-
-  Future<void> onLogin(String value) async {
-    await _secureStorage.write(key: constants.appName, value: value);
+    final emptyRequest = Empty();
+    final meResponse = await _client.me(emptyRequest);
+    return meResponse.user.userType != User_UserType.UNSPECIFIED;
   }
 
   Future<void> onLogout() async {
     await _secureStorage.delete(key: constants.appName);
-    _httpClient.dispose();
   }
 }
