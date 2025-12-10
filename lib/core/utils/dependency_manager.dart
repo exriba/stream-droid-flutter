2b279f30cfa222c280724cc2b1298d8859363f44
exriba <44509247+exriba@users.noreply.github.com>
2025-12-09 21:24:56 -0500

    Renamed previous redeem service to reward service

--- lib/core/utils/dependency_manager.dart
+++ lib/core/utils/dependency_manager.dart
@@ -3,6 +3,7 @@ import 'package:stream_droid_app/core/network/droid_client.dart';
 import 'package:stream_droid_app/core/utils/local_storage.dart';
 import 'package:stream_droid_app/core/utils/secure_storage.dart';
 import 'package:stream_droid_app/domain/services/auth_interceptor.dart';
+import 'package:stream_droid_app/domain/services/reward_service.dart';
 import 'package:stream_droid_app/domain/services/redeem_service.dart';
 import 'package:stream_droid_app/domain/services/user_service.dart';
 
@@ -21,8 +22,11 @@ class DependencyManager {
     getIt.registerSingleton<UserService>(
       UserService(getIt<ISecureStorage>(), getIt<AuthInterceptor>()),
     );
+    getIt.registerSingleton<RewardService>(
+      RewardService(getIt<IDroidClient>()),
+    );
     getIt.registerSingleton<RedeemService>(
-      RedeemService(getIt<IDroidClient>()),
+      RedeemService(getIt<AuthInterceptor>()),
     );
   }
 }
