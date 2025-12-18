73dd971c36cb67f0cbc48d5902ff506ecfcaf88d
exriba <44509247+exriba@users.noreply.github.com>
2025-12-17 23:10:17 -0500

    Update auth_interceptor.dart
    Updated AuthInterceptor to handle token renewal

--- lib/domain/services/auth_interceptor.dart
+++ lib/domain/services/auth_interceptor.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'package:grpc/grpc.dart';
 import 'package:stream_droid_app/core/utils/secure_storage.dart';
-import 'package:stream_droid_app/core/constants/constants.dart' as constants;
 
 class AuthInterceptor implements ClientInterceptor {
   AuthInterceptor(this._secureStorage);
@@ -13,25 +12,46 @@ class AuthInterceptor implements ClientInterceptor {
       Stream<Q> requests,
       CallOptions options,
       ClientStreamingInvoker<Q, R> invoker) {
-    return invoker(
-      method,
-      requests,
-      options.mergedWith(CallOptions(providers: [_provider])),
+    final newOptions = options.mergedWith(
+      CallOptions(providers: [_injectTokenProvider]),
     );
+
+    final call = invoker(method, requests, newOptions);
+
+    call.headers.then(_handleResponseMetadata);
+
+    return call;
   }
 
   @override
   ResponseFuture<R> interceptUnary<Q, R>(ClientMethod<Q, R> method, Q request,
       CallOptions options, ClientUnaryInvoker<Q, R> invoker) {
-    return invoker(
-      method,
-      request,
-      options.mergedWith(CallOptions(providers: [_provider])),
+    final newOptions = options.mergedWith(
+      CallOptions(providers: [_injectTokenProvider]),
     );
+
+    final call = invoker(method, request, newOptions);
+
+    call.headers.then(_handleResponseMetadata);
+
+    return call;
   }
 
-  FutureOr<void> _provider(Map<String, String> metadata, String uri) async {
-    final token = await _secureStorage.read(key: constants.appName);
-    metadata['Authorization'] = "Bearer $token";
+  FutureOr<void> _injectTokenProvider(
+      Map<String, String> metadata, String uri) async {
+    final token = await _secureStorage.getToken();
+    if (token != null) {
+      metadata['authorization'] = 'Bearer $token';
+    }
+  }
+
+  FutureOr<void> _handleResponseMetadata(Map<String, String> metadata) async {
+    final bearerToken = metadata['authorization'];
+    if (bearerToken != null) {
+      final token = bearerToken.startsWith('Bearer ')
+          ? bearerToken.substring(7)
+          : bearerToken;
+      await _secureStorage.saveToken(token: token);
+    }
   }
 }
