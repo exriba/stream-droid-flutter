a9909f4591c204d50311ccc345892cb02c6e9b99
exriba <44509247+exriba@users.noreply.github.com>
2026-03-17 22:18:50 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,30 @@
-import 'dart:ui';
 import 'dart:async';
-import 'package:localstorage/localstorage.dart';
-import 'package:talker/talker.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:media_kit/media_kit.dart';
-import 'package:stream_droid_app/app.dart';
-import 'package:window_manager/window_manager.dart';
-import 'package:stream_droid_app/core/utils/dependency_manager.dart';
+import 'package:stream_droid_app/src/app/app.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:stream_droid_app/src/services/window_service.dart';
+import 'package:stream_droid_app/src/constants/constants.dart' as constants;
 
-// TODO: Review app views and implement responsive design where necessary.
-const windowOptions = WindowOptions(
-    center: true,
-    title: "Stream Droid",
-    size: Size(900, 600),
-    minimumSize: Size(900, 600));
+void main() {
+  runZonedGuarded(
+    () async {
+      WidgetsFlutterBinding.ensureInitialized();
+      await WindowService.ensureInitialized();
+      await Hive.initFlutter();
+      await Hive.openBox(constants.settings);
+      MediaKit.ensureInitialized();
 
-Future<void> main() async {
-  FutureBuilder.debugRethrowError = true;
-  WidgetsFlutterBinding.ensureInitialized();
-  MediaKit.ensureInitialized();
-  DependencyManager.configure();
-  PlatformDispatcher.instance.onError = onError;
-
-  await initLocalStorage();
-  await windowManager.ensureInitialized();
-  windowManager.waitUntilReadyToShow(windowOptions, () async {
-    await windowManager.show();
-    await windowManager.focus();
-  });
-
-  runApp(const App());
-}
-
-// TODO: logs
-Talker? talker;
-bool onError(Object error, StackTrace stack) {
-  talker = talker ?? Talker();
-  talker?.handle(error, stack);
-  return true;
+      runApp(
+        const ProviderScope(
+          child: App(),
+        ),
+      );
+    },
+    (error, stackTrace) {
+      // TODO: Review error handling
+      debugPrint('Uncaught error in zone: $error');
+    },
+  );
 }
