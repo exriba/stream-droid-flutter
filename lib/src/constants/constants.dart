84395ec9e00b7141be6a6cc234bb6091e0a6eef0
exriba <44509247+exriba@users.noreply.github.com>
2026-03-17 22:09:08 -0400

    clean up constants

---
+++ lib/src/constants/constants.dart
@@ -0,0 +1,6 @@
+const String appName = String.fromEnvironment("app_name");
+const String serverName = String.fromEnvironment("server_name");
+const int serverPort = int.fromEnvironment("server_port");
+
+const String settings = "Settings";
+const String defaultVolumeKey = "defaultVolume";
