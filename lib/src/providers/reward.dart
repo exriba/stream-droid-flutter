5addf5257c5b398804b0ed4a9ac82d21e7b91182
exriba <44509247+exriba@users.noreply.github.com>
2026-03-19 16:30:15 -0400

    Update reward.dart
    * Prefer explicit declarations over generated code

--- lib/src/providers/reward.dart
+++ lib/src/providers/reward.dart
@@ -1,14 +1,31 @@
 import 'package:riverpod_annotation/riverpod_annotation.dart';
-import 'package:stream_droid_app/src/generated/service/rewardservice.pbgrpc.dart';
-import 'package:stream_droid_app/src/providers/rewards.dart';
+import 'package:stream_droid_app/src/controllers/reward_controller.dart';
+import 'package:stream_droid_app/src/generated/common/reward.pb.dart';
+import 'package:stream_droid_app/src/providers/auth_interceptor.dart';
+import 'package:stream_droid_app/src/providers/client_channel.dart';
+import 'package:stream_droid_app/src/services/reward_service.dart';
 
-part 'reward.g.dart';
+final rewardServiceProvider = Provider<RewardService>((ref) {
+  final clientChannel = ref.read(clientChannelProvider);
+  final authInterceptor = ref.read(authInterceptorProvider);
+  return RewardService(clientChannel, authInterceptor);
+});
 
-@riverpod
-class Reward extends _$Reward {
+final rewardControllerProvider = Provider<RewardController>((ref) {
+  final rewardService = ref.read(rewardServiceProvider);
+  return RewardController(rewardService);
+});
+
+final rewardNotificationProvider =
+    AsyncNotifierProvider.autoDispose<RewardNotifier, List<Reward>>(
+  RewardNotifier.new,
+);
+
+class RewardNotifier extends AutoDisposeAsyncNotifier<List<Reward>> {
   @override
-  Future<RewardResponse> build(String rewardId) async {
+  Future<List<Reward>> build() async {
     final service = ref.read(rewardServiceProvider);
-    return service.fetchReward(rewardId);
+    final responses = await service.fetchRewards().toList();
+    return responses.map((response) => response.reward).toList();
   }
 }
