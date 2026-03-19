709057b0089c5a19dae7ed79cafadd34e7a6e554
exriba <44509247+exriba@users.noreply.github.com>
2026-03-19 19:27:55 -0400

    Implement add and remove action handlers

--- lib/src/widgets/reward_asset_list.dart
+++ lib/src/widgets/reward_asset_list.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:stream_droid_app/src/generated/common/reward.pb.dart';
+import 'package:stream_droid_app/src/providers/hive_storage.dart';
+import 'package:stream_droid_app/src/providers/reward.dart';
 import 'package:stream_droid_app/src/widgets/reward_asset.dart';
+import 'package:stream_droid_app/src/constants/constants.dart' as constants;
 
-class RewardAssetList extends ConsumerWidget {
+class RewardAssetList extends ConsumerStatefulWidget {
   const RewardAssetList({
     super.key,
     required this.rewardId,
@@ -13,18 +16,62 @@ class RewardAssetList extends ConsumerWidget {
   final List<Asset> rewardAssets;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<ConsumerStatefulWidget> createState() => _RewardAssetList();
+}
+
+class _RewardAssetList extends ConsumerState<RewardAssetList> {
+  List<Asset> assets = [];
+
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      assets = widget.rewardAssets;
+    });
+  }
+
+  Future<void> _handleAdd() async {
+    final service = ref.read(rewardServiceProvider);
+    final storage = ref.read(hiveStorageProvider);
+    final defaultVolume = storage.get<double>(
+      constants.defaultVolumeKey,
+      defaultValue: 50,
+    );
+
+    final volume = defaultVolume.toInt();
+    final rewardAssets = await service.addRewardAssets(widget.rewardId, volume);
+    if (rewardAssets.isNotEmpty) {
+      setState(() {
+        assets = rewardAssets;
+      });
+    }
+  }
+
+  Future<void> _handleRemove(Asset asset) async {
+    final service = ref.read(rewardServiceProvider);
+    await service.deleteRewardAsset(widget.rewardId, asset.fileName);
+    final removed = assets.remove(asset);
+    if (removed) {
+      setState(() {
+        assets = [...assets];
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.transparent,
       body: ListView.separated(
         padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 8),
-        itemCount: rewardAssets.length,
+        itemCount: assets.length,
         itemBuilder: (context, index) {
+          final asset = assets[index];
           return RewardAsset(
-            key: Key(rewardAssets[index].id),
-            asset: rewardAssets[index],
-            rewardId: rewardId,
-            handleRemove: (fileName) async => {},
+            key: Key(asset.id),
+            asset: asset,
+            rewardId: widget.rewardId,
+            handleRemove: _handleRemove,
           );
         },
         separatorBuilder: (context, index) {
@@ -41,8 +88,8 @@ class RewardAssetList extends ConsumerWidget {
           Icons.add,
           color: Colors.white,
         ),
-        onPressed: () {
-          // TODO: Implement this
+        onPressed: () async {
+          await _handleAdd();
         },
       ),
     );
