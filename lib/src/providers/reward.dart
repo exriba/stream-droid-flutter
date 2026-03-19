7eb53990d9144dec2144d3c9f971a6e26451486f
exriba <44509247+exriba@users.noreply.github.com>
2026-03-19 17:38:25 -0400

    Update reward.dart

--- lib/src/providers/reward.dart
+++ lib/src/providers/reward.dart
@@ -1,5 +1,4 @@
 import 'package:riverpod_annotation/riverpod_annotation.dart';
-import 'package:stream_droid_app/src/controllers/reward_controller.dart';
 import 'package:stream_droid_app/src/generated/common/reward.pb.dart';
 import 'package:stream_droid_app/src/providers/auth_interceptor.dart';
 import 'package:stream_droid_app/src/providers/client_channel.dart';
@@ -9,11 +8,6 @@ final rewardServiceProvider = Provider<RewardService>((ref) {
   final clientChannel = ref.read(clientChannelProvider);
   final authInterceptor = ref.read(authInterceptorProvider);
   return RewardService(clientChannel, authInterceptor);
-});
-
-final rewardControllerProvider = Provider<RewardController>((ref) {
-  final rewardService = ref.read(rewardServiceProvider);
-  return RewardController(rewardService);
 });
 
 final rewardNotificationProvider =
