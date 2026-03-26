00cdcc14faa5ebf8db0782a15741e385a6f415be
exriba <44509247+exriba@users.noreply.github.com>
2026-03-26 09:40:46 -0400

    Create grpc_error_handler.dart
    * This class will be responsible for handling grpc errors.
    * I may need the correlation id to track issues in the backend as well

---
+++ lib/src/utils/grpc_error_handler.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:grpc/grpc.dart';
+import 'package:stream_droid_app/src/providers/global_error.dart';
+
+class GrpcErrorHandler {
+  static void handleError(ProviderContainer container, dynamic error) {
+    String message = "Unexpected error";
+
+    if (error is GrpcError) {
+      message = _mapGrpcError(error);
+    }
+
+    final errorProvider = container.read(globalErrorProvider.notifier);
+    errorProvider.state = message;
+
+    // TODO: Log this
+    debugPrint('Error caught: $error');
+  }
+
+  static String _mapGrpcError(GrpcError error) {
+    switch (error.code) {
+      case StatusCode.cancelled:
+        return "Request cancelled.";
+      case StatusCode.unknown:
+        return "Unknown error.";
+      case StatusCode.invalidArgument:
+        return "Invalid request.";
+      case StatusCode.deadlineExceeded:
+        return "Request timeout.";
+      case StatusCode.notFound:
+        return "Not found.";
+      case StatusCode.permissionDenied:
+        return "Permission denied.";
+      case StatusCode.unauthenticated:
+        return "Session expired.";
+      case StatusCode.unavailable:
+        return "Service unavailable";
+      default:
+        return error.message ?? "Unexpected error";
+    }
+  }
+}
