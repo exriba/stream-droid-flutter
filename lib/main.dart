8cb925b0e6e7b13662cdeeae0848b11d2b51a713
exriba <44509247+exriba@users.noreply.github.com>
2026-03-26 14:52:25 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,24 +7,16 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:stream_droid_app/src/services/window_service.dart';
 import 'package:stream_droid_app/src/constants/constants.dart' as constants;
 
-void main() {
-  runZonedGuarded(
-    () async {
-      WidgetsFlutterBinding.ensureInitialized();
-      await WindowService.ensureInitialized();
-      await Hive.initFlutter();
-      await Hive.openBox(constants.settings);
-      MediaKit.ensureInitialized();
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await WindowService.ensureInitialized();
+  await Hive.initFlutter();
+  await Hive.openBox(constants.settings);
+  MediaKit.ensureInitialized();
 
-      runApp(
-        const ProviderScope(
-          child: App(),
-        ),
-      );
-    },
-    (error, stackTrace) {
-      // TODO: Review error handling
-      debugPrint('Uncaught error in zone: $error');
-    },
+  runApp(
+    const ProviderScope(
+      child: App(),
+    ),
   );
 }
