cc2abf83de90be89ae397cd515b81bb667c7e903
exriba <44509247+exriba@users.noreply.github.com>
2026-03-29 16:49:49 -0400

    Update app_test.dart
    * Added test cases to ensure other screens (Media, Statistics and Settings) render properly

--- test/src/app/app_test.dart
+++ test/src/app/app_test.dart
@@ -1,35 +1,77 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:stream_droid_app/src/app/app.dart';
+import 'package:stream_droid_app/src/generated/common/redeem.pb.dart';
 import 'package:stream_droid_app/src/generated/common/reward.pb.dart';
+import 'package:stream_droid_app/src/providers/hive_storage.dart';
+import 'package:stream_droid_app/src/providers/redeem.dart';
 import 'package:stream_droid_app/src/providers/reward.dart';
 import 'package:stream_droid_app/src/providers/secure_storage.dart';
 import 'package:stream_droid_app/src/screens/dashboard_screen.dart';
 import 'package:stream_droid_app/src/screens/login_screen.dart';
+import 'package:stream_droid_app/src/screens/media_screen.dart';
+import 'package:stream_droid_app/src/screens/settings_screen.dart';
+import 'package:stream_droid_app/src/screens/statistics_screen.dart';
+import 'package:stream_droid_app/src/services/hive_storage.dart';
 import 'package:stream_droid_app/src/services/secure_storage.dart';
 
 class MockSecureStorage extends Mock implements SecureStorage {}
 
-class FakeRewardNotifier extends RewardNotifier {
-  FakeRewardNotifier(List<Reward> fakeRewards) : _fakeRewards = fakeRewards;
-  final List<Reward> _fakeRewards;
+class MockHiveStorage extends Mock implements HiveStorage {}
 
+class FakeRewardNotifier extends RewardNotifier {
   @override
   Future<List<Reward>> build() async {
-    return _fakeRewards;
+    return <Reward>[];
+  }
+}
+
+class FakeRedeemNotifier extends RedeemNotifier {
+  @override
+  Future<List<RewardRedeem>> build() async {
+    return <RewardRedeem>[];
   }
 }
 
 void main() {
   group('App', () {
+    late ProviderContainer container;
     late MockSecureStorage mockStorage;
+    late MockHiveStorage mockHiveStorage;
 
     setUp(() {
       mockStorage = MockSecureStorage();
+      mockHiveStorage = MockHiveStorage();
+      container = ProviderContainer(
+        overrides: [
+          secureStorageProvider.overrideWithValue(mockStorage),
+          hiveStorageProvider.overrideWithValue(mockHiveStorage),
+          rewardNotificationProvider.overrideWith(
+            () => FakeRewardNotifier(),
+          ),
+          redeemNotificationProvider.overrideWith(
+            () => FakeRedeemNotifier(),
+          ),
+        ],
+      );
+
+      when(() => mockStorage.getToken()).thenAnswer((_) async => 'valid_token');
+
+      when(
+        () => mockHiveStorage.get<double>(
+          any(),
+          defaultValue: any(named: 'defaultValue'),
+        ),
+      ).thenAnswer((_) => 50);
     });
 
-    testWidgets('redirects to login if token is null', (tester) async {
+    tearDown(() {
+      container.dispose();
+    });
+
+    testWidgets('renders login screen if token is null', (tester) async {
       // Arrange
       when(() => mockStorage.getToken()).thenAnswer((_) async => null);
 
@@ -51,18 +93,7 @@ void main() {
       expect(find.byType(DashboardScreen), findsNothing);
     });
 
-    testWidgets('redirects to dashboard if token exists', (tester) async {
-      when(() => mockStorage.getToken()).thenAnswer((_) async => 'valid_token');
-
-      final container = ProviderContainer(
-        overrides: [
-          secureStorageProvider.overrideWithValue(mockStorage),
-          rewardNotificationProvider.overrideWith(
-            () => FakeRewardNotifier(<Reward>[]),
-          ),
-        ],
-      );
-
+    testWidgets('renders dashboard screen if token exists', (tester) async {
       await tester.pumpWidget(
         UncontrolledProviderScope(
           container: container,
@@ -72,9 +103,62 @@ void main() {
 
       await tester.pumpAndSettle();
 
-      // Assert that DashboardScreen is displayed
-      expect(find.byType(DashboardScreen), findsOneWidget);
       expect(find.byType(LoginScreen), findsNothing);
+      expect(find.byType(DashboardScreen), findsOneWidget);
+    });
+
+    testWidgets('renders media screen', (tester) async {
+      await tester.pumpWidget(
+        UncontrolledProviderScope(
+          container: container,
+          child: const App(),
+        ),
+      );
+
+      await tester.pumpAndSettle();
+
+      final nagivationbutton = find.byIcon(Icons.live_tv_outlined);
+
+      await tester.tap(nagivationbutton);
+      await tester.pumpAndSettle();
+
+      expect(find.byType(MediaScreen), findsOneWidget);
+    });
+
+    testWidgets('renders statistics screen', (tester) async {
+      await tester.pumpWidget(
+        UncontrolledProviderScope(
+          container: container,
+          child: const App(),
+        ),
+      );
+
+      await tester.pumpAndSettle();
+
+      final nagivationbutton = find.byIcon(Icons.show_chart_outlined);
+
+      await tester.tap(nagivationbutton);
+      await tester.pumpAndSettle();
+
+      expect(find.byType(StatisticsScreen), findsOneWidget);
+    });
+
+    testWidgets('renders settings screen', (tester) async {
+      await tester.pumpWidget(
+        UncontrolledProviderScope(
+          container: container,
+          child: const App(),
+        ),
+      );
+
+      await tester.pumpAndSettle();
+
+      final nagivationbutton = find.byIcon(Icons.settings_outlined);
+
+      await tester.tap(nagivationbutton);
+      await tester.pumpAndSettle();
+
+      expect(find.byType(SettingsScreen), findsOneWidget);
     });
   });
 }
